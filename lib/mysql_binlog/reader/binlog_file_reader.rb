9c61635431b4de543298193c9001fccfa5bc14fe
Jeremy Cole <jeremy@jcole.us>
2012-06-25 09:42:17 -0700

    Remove accidentally left-in debug code

--- lib/mysql_binlog/reader/binlog_file_reader.rb
+++ lib/mysql_binlog/reader/binlog_file_reader.rb
@@ -52,7 +52,6 @@ module MysqlBinlog
     def read(length)
       return "" if length == 0
       data = @binlog.read(length)
-      puts_hex data
       if !data
         raise MalformedBinlogException.new
       elsif data.length == 0
